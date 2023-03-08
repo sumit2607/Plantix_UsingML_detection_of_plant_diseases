02d609ef47e80cb02480a01cb214732efaddb1c4
sumitrai <developer2607@gmail.com>
2023-03-08 20:05:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/plantix_usingml_detection_of_plant_diseases/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.plantix_usingml_detection_of_plant_diseases
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
